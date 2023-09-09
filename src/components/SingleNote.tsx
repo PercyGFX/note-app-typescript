378096f15acb2a197ecd1663ea3159c201e4f89f
PercyGFX <isurangabtk@gmail.com>
2023-09-10 03:43:57 +0530

    icons added to single items

--- src/components/SingleNote.tsx
+++ src/components/SingleNote.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import {Note} from "../services/model"
+import { AiTwotoneDelete, AiFillEdit } from 'react-icons/ai';
+import { TiTick } from 'react-icons/ti';
 
 interface Props {
     note: Note,
@@ -12,8 +14,21 @@ const SingleNote = ({note,key, notes, setNotes}: Props)=> {
 
     return (
 
-        <div>
-            <p> {note.note}</p>
+        <div className="flex justify-center">
+
+            <div className="bg-amber-100 shadow-2xl m-3 rounded p-5 w-[1000px]">
+                <div>
+                    {note.note}
+                </div>
+
+                <div className="flex text-2xl items-start mt-2">
+                    <AiTwotoneDelete className="mx-2" />
+                    <AiFillEdit className="mx-2" />
+                    <TiTick className="mx-2" />
+                </div>
+
+            </div>
+
         </div>
     )
 
