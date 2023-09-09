6222a6a659f4aa096abaf1c0e94eaef0da68be1c
PercyGFX <isurangabtk@gmail.com>
2023-09-09 16:42:58 +0530

    tailwind install + initial componenets

---
+++ src/components/InputField.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const InputField = ()=> {
+
+    return (
+        <div>
+            <p>hghghf </p>
+        </div>
+    )
+
+}
+
+export default InputField
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+      <div className="bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-sky-400 to-blue-800 min-h-screen">
     <App />
+      </div>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
